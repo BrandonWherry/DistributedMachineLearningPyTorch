ba131471ab02044f4c8f365c480902ef026d3b30
Brandon <bwh1126@gmail.com>
2022-11-30 00:23:39 -0600

    repo adjustments

--- efs_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo mkdir efs
-sudo apt-get -y install git binutils
-git clone https://github.com/aws/efs-utils
-cd ./efs-utils
-./build-deb.sh
-sudo apt-get -y install ./build/amazon-efs-utils*deb
