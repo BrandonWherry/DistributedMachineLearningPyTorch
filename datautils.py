9f6f95ccf3a3ffd111b70b6c213289f0cae1dcea
Brandon <bwh1126@gmail.com>
2022-11-29 19:23:34 -0600

    ..

---
+++ datautils.py
@@ -0,0 +1,18 @@
+import torch
+from torch.utils.data import Dataset
+
+class MyTrainDataset(Dataset):
+    """Random Dataset meant for testing, obtained from
+       https://github.com/pytorch/examples/blob/main/distributed/ddp-tutorial-series/datautils.py
+    Args:
+        Dataset (_type_): _description_
+    """
+    def __init__(self, size):
+        self.size = size
+        self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
+
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, index):
+        return self.data[index]
