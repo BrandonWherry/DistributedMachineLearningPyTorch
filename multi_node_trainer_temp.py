f7d78d47b8b3d90935fc4fcaa881e21a8b6cf1c5
Brandon <bwh1126@gmail.com>
2022-11-30 04:05:45 -0600

    added debug prints

--- multi_node_trainer_temp.py
+++ multi_node_trainer_temp.py
@@ -95,7 +95,8 @@ class Trainer:
 
     def _run_epoch(self, epoch):
         b_sz = len(next(iter(self.train_data))[0])
-        print(f"[GPU{self.global_rank}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        print(f"[GPU{self.global_rank}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)} \
+            | Best Validation Loss: {self.lowest_loss}")
         self.train_data.sampler.set_epoch(epoch)
         train_loss = 0
         valid_loss = 0
@@ -115,6 +116,7 @@ class Trainer:
         # Update loss histories
         self.train_loss_history.append(train_loss/len(self.train_data))
         self.valid_loss_history.append(valid_loss/len(self.valid_data))
+        print('Finished Epoch')
 
 
     def _save_snapshot(self, epoch: int):
