0fc96a39cfe4b974ce967e85fcab219c15279e30
Brandon <bwh1126@gmail.com>
2022-12-01 00:15:30 -0600

    added debug code

--- multi_node_trainer.py
+++ multi_node_trainer.py
@@ -14,6 +14,7 @@
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
+import torch.multiprocessing as mp
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
