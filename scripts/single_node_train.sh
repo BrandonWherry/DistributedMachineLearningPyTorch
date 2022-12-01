391717b12a0c6f9eed574e36633a7fbfb8abae53
Brandon <bwh1126@gmail.com>
2022-12-01 03:38:51 -0600

    b_sz fix

--- scripts/single_node_train.sh
+++ scripts/single_node_train.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 train_time=${1:-0.25}
 model_name=${2:-model}
-batch_size=${3:-64}
+b_sz=${3:-64}
 
 # Executing torchrun on a single worker
 torchrun                        \
@@ -10,7 +10,7 @@ torchrun                        \
 multi_node_trainer.py           \
 --train_time="$train_time"      \
 --model_name="$model_name.pt"   \
---batch_size="$batch_size"      \
+--batch_size="$b_sz"      \
 2>&1 | tee "training_saves/$model_name.log"
 
 
